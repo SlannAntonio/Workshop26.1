66f7b376b7addf0563230a100995920583a5cce2
SlannAntonio <slann.antonio12@gmail.com>
2026-03-17 16:53:52 -0300

    Update Curso de idiomas.sql
    Correção no código

--- Curso de idiomas.sql
+++ Curso de idiomas.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Aluno (
     Id_aluno INT PRIMARY KEY,
-    Nome CHAR(100),
+    Nome VARCHAR(100),
     Data_nasc DATE,
     E_mail VARCHAR(100),
     CPF VARCHAR(14)
@@ -8,7 +8,7 @@ CREATE TABLE Aluno (
 
 CREATE TABLE Professor (
     Id_professor INT PRIMARY KEY,
-    Nome CHAR(100),
+    Nome VARCHAR(100),
     CPF VARCHAR(14),
     E_Mail VARCHAR(100),
     Telefone VARCHAR(20)
@@ -17,7 +17,7 @@ CREATE TABLE Professor (
 CREATE TABLE Turma (
     Id_turma INT PRIMARY KEY,
     Periodo VARCHAR(50),
-    Turno CHAR(20),
+    Turno VARCHAR(20),
     Id_aluno INT,
     Id_professor INT,
     FOREIGN KEY (Id_aluno) REFERENCES Aluno(Id_aluno),
@@ -26,8 +26,8 @@ CREATE TABLE Turma (
 
 CREATE TABLE Curso (
     Id_curso INT PRIMARY KEY,
-    Idioma CHAR(50),
-    Nivel CHAR(50),
+    Idioma VARCHAR(50),
+    Nivel VARCHAR(50),
     Id_turma INT,
     FOREIGN KEY (Id_turma) REFERENCES Turma(Id_turma)
 );
@@ -35,7 +35,7 @@ CREATE TABLE Curso (
 CREATE TABLE Matricula (
     Id_matricula INT PRIMARY KEY,
     Data_mat DATE,
-    Status CHAR(20),
+    Status VARCHAR(20),
     Id_aluno INT,
     FOREIGN KEY (Id_aluno) REFERENCES Aluno(Id_aluno)
 );
@@ -46,7 +46,7 @@ CREATE TABLE Pagamento (
     Desconto DECIMAL(10,2),
     Data_venc DATE,
     Valor DECIMAL(10,2),
-    Status CHAR(20),
+    Status VARCHAR(20),
     Id_matricula INT,
     FOREIGN KEY (Id_matricula) REFERENCES Matricula(Id_matricula)
 );
